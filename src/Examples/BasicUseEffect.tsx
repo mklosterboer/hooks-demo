9bac34478e09aa4ebd1b46908b337589049142ad
Michael Klosterboer <michael.klosterboer@hcss.com>
2019-05-14 08:00:26 -0500

    Add cleanup function to BasicUseEffect

--- src/Examples/BasicUseEffect.tsx
+++ src/Examples/BasicUseEffect.tsx
@@ -8,29 +8,23 @@ const BasicUseEffect = () => {
     useEffect(() => {
         document.title = `Counter 1: ${count1}`;
         setUpdatedTime((new Date()).toLocaleTimeString());
+
+        return () => { document.title = 'React Hooks Demo' };
     }, [count1]);
-
-    const handleClick = () => {
-        setCount1(count1 + 1);
-    }
-
-    const handleClick2 = () => {
-        setCount2(count2 + 1);
-    }
 
     return (
         <>
             <span style={{ textAlign: "center" }}>Title last updated: {updatedTime}</span>
             <div className="counter-container">
-                <button onClick={handleClick}>
+                <button onClick={() => { setCount1(count1 + 1) }}>
                     Click me
-            </button>
+                </button>
                 Counter 1: {count1}
             </div>
             <div className="counter-container">
-                <button onClick={handleClick2}>
+                <button onClick={() => { setCount2(count1 + 1) }}>
                     Click me
-            </button>
+                </button>
                 Counter 2: {count2}
             </div>
         </>
@@ -42,7 +36,8 @@ const basicUseEffectDesc = (
         useEffect fires every time the component is re-rendered, unless a dependecy array is passed. <br /><br />
         In this example, the title keeps track of Counter 1. Since this effect only cares if the value of Counter 1 has been updated,
         we pass 'count1' in an array to useEffect. <br /><br />
-        As the component continues to render, when we increase Counter 2, we do not run the potentially expensive calculation in the useEffect, unless 'count1' changes between renders.
+        As the component continues to render, when we increase Counter 2, we do not run the potentially expensive calculation in the useEffect, unless 'count1' changes between renders.<br /><br />
+        The title is also set back to 'React Hooks Demo' when this component is unmounted thanks to a returned function in useEffect.
     </div>
 );
 
